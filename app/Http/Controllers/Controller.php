d2628dc32fb9221e73aceef8d36d47e714d9ae04
Ko Ye <redboy.mkn@gmail.com>
2022-12-27 21:16:58 +0630

    dashboard install

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,9 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function dashboard(){
+        return "hello";
+        // return view('dashboard.pages.home');
+    }
 }
